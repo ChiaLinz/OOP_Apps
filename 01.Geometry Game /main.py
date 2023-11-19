4c05414e43c1cee56a06a890926f6e03aa84fedf
Chia-Lin <93101704+ChiaLinz@users.noreply.github.com>
2023-11-19 21:08:43 +0800

    add geometry_game

--- 01.Geometry Game /main.py
+++ 01.Geometry Game /main.py
@@ -1,4 +1,4 @@
-"""Add a new distance method to the Point class. The method should calculate the distance from the coordinates of the current point (i.e., the self.x and self.y coordinates) to the coordinates of any other given point, and such coordinates can be provided as x and y arguments to the distance method."""
+from random import randint
 
 class Point:
 
@@ -6,12 +6,40 @@ class Point:
         self.x = x
         self.y = y
 
-
-    def falls_in_rectangle(self, lowleft, upright):
-        if lowleft[0] < self.x < upright[0] and lowleft[1] < self.y < upright[1]:
+    def falls_in_rectangle(self, rectangle):
+        if rectangle.point1.x < self.x < rectangle.point2.x \
+                and rectangle.point1.y < self.y < rectangle.point2.y:
             return True
         else:
             return False
 
-    def distance_from_point(self, x, y):
-        return ((self.x - x)**2 + (self.y - y)**2) ** 0.5
+
+class Rectangle:
+
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def area(self):
+        return (self.point2.x - self.point1.x) * \
+               (self.point2.y - self.point1.y)
+
+
+# Create rectangle object
+rectangle = Rectangle(Point(randint(0, 9), randint(0, 9)),
+              Point(randint(10, 19), randint(10, 19)))
+
+# Print rectangle coordinates
+print("Rectangle Coordinates: ",
+      rectangle.point1.x, ",",
+      rectangle.point1.y, "and",
+      rectangle.point2.x, ",",
+      rectangle.point2.y)
+
+# Get point and area from user
+user_point = Point(float(input("Guess x: ")), float(input("Guess y: ")))
+user_area = float(input("Guess rectangle area: "))
+
+# Print out the game result
+print("Your point was inside rectangle: ", user_point.falls_in_rectangle(rectangle))
+print("Your area was off by: ", rectangle.area() - user_area)
