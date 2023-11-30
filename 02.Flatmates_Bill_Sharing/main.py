c3630384f0c113b60ec9ccbfa3ec46071edc3949
Chia-Lin <93101704+ChiaLinz@users.noreply.github.com>
2023-11-30 17:28:28 +0800

    add input

--- 02.Flatmates_Bill_Sharing/main.py
+++ 02.Flatmates_Bill_Sharing/main.py
@@ -64,12 +64,20 @@ class PdfReport:
         pdf.output(self.filename)
         webbrowser.open('file://' + os.path.realpath((self.filename)))
 
-the_bill = Bill(amount = 120, period = "April 2021")
-john = Flatmate(name = "John", days_in_house = 20)
-marry = Flatmate(name = "Marry", days_in_house = 25)
+amount = float(input("Hey user, enter the bill amount: "))
+period = input("What is the bill period? E.g. December 2020 ")
 
-print("John pays: ", john.pays(bill = the_bill, flatmate2 = marry))
-print("Marry pays: ", marry.pays(bill = the_bill, flatmate2 = john))
+name1 = input("What is your name? ")
+days_in_house1 = int(input(f"How many days did {name1} stay in the house during the bill period?"))
+name2 = input("What is the name of the other? ")
+days_in_house2 = int(input(f"How many days did {name2} stay in the house during the bill period?"))
 
-pdf_report = PdfReport(filename="Report1.pdf")
-pdf_report.generate(flatmate1=john, flatmate2=marry, bill=the_bill)
+the_bill = Bill(amount = amount, period = period)
+flatmate1 = Flatmate(name = name1, days_in_house = days_in_house1)
+flatmate2 = Flatmate(name = name2, days_in_house = days_in_house2)
+
+print(f"{name1} pays: ", flatmate1.pays(bill = the_bill, flatmate2 = flatmate2))
+print(f"{name2} pays: ", flatmate2.pays(bill = the_bill, flatmate2 = flatmate1))
+
+pdf_report = PdfReport(filename=f"{the_bill.period}.pdf")
+pdf_report.generate(flatmate1=flatmate1, flatmate2=flatmate2, bill=the_bill)
